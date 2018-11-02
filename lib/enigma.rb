27ac546b94b7e1167c35964e685fd907cabe1b2a
SyntheticAutomation <mclampet@msudenver.edu>
2018-11-02 00:00:23 -0600

    Fixes bugs with encryption, and adjusts method for scalability

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -6,21 +6,31 @@ class Enigma
   def initialize
     @key = ""
     @date = ""
+    @rotations = 0
+    @encrypted_message = ""
+    @set = ("a".."z").to_a << " "
   end
 
-  def encrypt(message_string, key = nil, date = Date.today)
+  def encrypt(message_string, key = nil, date = nil)
     @key << key
-    formatted_date = date.strftime("%d%m%y")
-    @date << formatted_date
-    # letter_encryption(message_string)
-    #returns {:encryption => x, :key => key, :date => formatted_date}
+    if date == nil
+      date = Date.today
+      formatted_date = date.strftime("%d%m%y")
+      @date << formatted_date
+    else
+      @date << date
+    end
+    create_keys
+    create_offsets
+    final_shift
+    {encryption: letter_encryption(message_string), key: key, date: @date}
   end
 
-  def distribute_keys
-    a = (@key.chars[0] + @key.chars[1]).to_i
-    b = (@key.chars[1] + @key.chars[2]).to_i
-    c = (@key.chars[2] + @key.chars[3]).to_i
-    d = (@key.chars[3] + @key.chars[4]).to_i
+  def create_keys
+    a = @key.chars[0] + @key.chars[1]
+    b = @key.chars[1] + @key.chars[2]
+    c = @key.chars[2] + @key.chars[3]
+    d = @key.chars[3] + @key.chars[4]
     [a, b, c, d]
   end
 
@@ -35,40 +45,38 @@ class Enigma
   end
 
   def final_shift
-    distribute_keys
-    create_offsets
-    keys_and_offsets = [distribute_keys, create_offsets]
-    shift_array = keys_and_offsets.transpose.map {|pair| pair.sum}
+    keys_as_integers = create_keys.map {|key| key.to_i}
+    keys_and_offsets = [keys_as_integers, create_offsets]
+    keys_and_offsets.transpose.map {|pair| pair.sum}
+  end
+
+  def rotate_once
+    @rotations += 1
   end
 
   def letter_encryption(message_string)
-    character_set = ("a".."z").to_a << " "
-    expanded_message_array = message_string.chars
-    x = expanded_message_array.map do |character|
-      shift_numbers = final_shift
-      location = character_set.index(character)
-      if character_set.include?(character)
-        new_letter = character_set[location - (27 - shift_numbers.first)] if shift_numbers.first < 27
-        new_letter = character_set[location - (shift_numbers.first - 27)] if shift_numbers.first > 27
-        shift_numbers.rotate!
-        new_letter
+    message_string.chars.map do |character|
+      recognized = @set.include?(character)
+      location = @set.index(character)
+      rotated_shifts = final_shift.rotate(@rotations)
+      if recognized && rotated_shifts.first < 27
+        rotate_once
+        index = location + rotated_shifts.first
+        new_letter = @set[index] if index < 27
+        new_letter = @set[index-27] if index > 27
+        @encrypted_message << new_letter
+      elsif recognized && rotated_shifts.first > 27
+        rotate_once
+        @encrypted_message << @set[location - (27 - (rotated_shifts.first % 27))]
+      elsif recognized && rotated_shifts.first == 27
+        rotate_once
+        @encrypted_message << @set[location]
       else
-        character
+        @encrypted_message << character
       end
     end
+    @encrypted_message
   end
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
